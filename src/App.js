518a6f735e4cfcb9f680bfab3c0a4390b0b857ae
Sidharth Jain <sidharthjainp9@gmail.com>
2024-09-23 14:15:16 +0530

    React basics/ import export + completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 import './App.css';
 
 
 export default function App() {
   return (
-    <div>App</div>
+    // <>App</> //OR below div one - have to wrapped in empty tags-ragments or div
+    // <div>App</div>
+    // naming convention - camelCase
+    <>
+      <Header />
+      <div>
+        <h1>Sids</h1>
+      </div>
+      <Footer />
+    </>
   )
 }
